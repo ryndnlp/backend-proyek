70911cbadb45345ed82ee90b21ca82e351330247
ryndnlp-tiket <ryan.daniel@tiket.com>
2022-03-03 18:31:36 +0700

    fix daily total per price

--- controllers/admin/summaryController.js
+++ controllers/admin/summaryController.js
@@ -57,183 +57,185 @@ const dailyTotalWeight = async (req, res) => {
 };
 
 const conclusionDashboard = async (req, res) => {
-  // try {
-  const { error, value } = conclusionDashboardSchema.validate(req.query);
-  if (error) {
-    const err = { name: error.name, ...error.details[0] };
-    throw err;
+  try {
+    const { error, value } = conclusionDashboardSchema.validate(req.query);
+    if (error) {
+      const err = { name: error.name, ...error.details[0] };
+      throw err;
+    }
+    const { currentDate } = value;
+
+    const nextDay = 1 * 24 * 60 * 60000;
+    const month = currentDate.getMonth() + 1;
+
+    const dailyTotalWeight = await Order.aggregate([
+      {
+        $match: {
+          orderDate: {
+            $gte: currentDate,
+            $lte: new Date(currentDate.getTime() + nextDay),
+          },
+        },
+      },
+      { $unwind: "$trashDetail" },
+      {
+        $group: {
+          _id: {
+            $dateToString: { format: "%m-%d-%Y", date: "$orderDate" },
+          },
+          sumWeight: { $sum: "$trashDetail.category" },
+        },
+      },
+    ]);
+
+    const dailyTotalFinishedWeight = await Order.aggregate([
+      {
+        $match: {
+          orderDate: {
+            $gte: currentDate,
+            $lte: new Date(currentDate.getTime() + nextDay),
+          },
+          orderStatus: "FINISH",
+        },
+      },
+      { $unwind: "$trashDetail" },
+      {
+        $group: {
+          _id: {
+            $dateToString: { format: "%m-%d-%Y", date: "$orderDate" },
+          },
+          sumWeight: { $sum: "$trashDetail.category" },
+        },
+      },
+    ]);
+
+    const dailyTotalInorganicWeight = await Order.aggregate([
+      {
+        $match: {
+          orderDate: {
+            $gte: currentDate,
+            $lte: new Date(currentDate.getTime() + nextDay),
+          },
+        },
+      },
+      { $unwind: "$trashDetail" },
+      {
+        $match: {
+          "trashDetail.type": {
+            $ne: "Organik",
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $dateToString: { format: "%m-%d-%Y", date: "$orderDate" },
+          },
+          sumWeight: { $sum: "$trashDetail.category" },
+        },
+      },
+    ]);
+
+    const monthlyTotalWeight = await Order.aggregate([
+      { $unwind: "$trashDetail" },
+      {
+        $group: {
+          _id: {
+            $month: "$orderDate",
+          },
+          sumWeight: { $sum: "$trashDetail.category" },
+        },
+      },
+      {
+        $match: {
+          _id: month,
+        },
+      },
+    ]);
+
+    const monthlyTotalFinishedWeight = await Order.aggregate([
+      {
+        $match: {
+          orderStatus: "FINISH",
+        },
+      },
+      { $unwind: "$trashDetail" },
+      {
+        $group: {
+          _id: {
+            $month: "$orderDate",
+          },
+          sumWeight: { $sum: "$trashDetail.category" },
+        },
+      },
+      {
+        $match: {
+          _id: month,
+        },
+      },
+    ]);
+
+    const monthlyTotalInorganicWeight = await Order.aggregate([
+      { $unwind: "$trashDetail" },
+      {
+        $match: {
+          "trashDetail.type": {
+            $ne: "Organik",
+          },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            $month: "$orderDate",
+          },
+          sumWeight: { $sum: "$trashDetail.category" },
+        },
+      },
+      {
+        $match: {
+          _id: month,
+        },
+      },
+    ]);
+
+    const result = {
+      daily: {
+        totalWeight: dailyTotalWeight.length
+          ? dailyTotalWeight[0].sumWeight
+          : 0,
+        totalFinishedWeight: dailyTotalFinishedWeight.length
+          ? dailyTotalFinishedWeight[0].sumWeight
+          : 0,
+        totalInorganicWeight: dailyTotalInorganicWeight.length
+          ? dailyTotalInorganicWeight[0].sumWeight
+          : 0,
+      },
+      monthly: {
+        totalWeight: monthlyTotalWeight.length
+          ? monthlyTotalWeight[0].sumWeight
+          : 0,
+        totalFinishedWeight: monthlyTotalFinishedWeight.length
+          ? monthlyTotalFinishedWeight[0].sumWeight
+          : 0,
+        totalInorganicWeight: monthlyTotalInorganicWeight.length
+          ? monthlyTotalInorganicWeight[0].sumWeight
+          : 0,
+      },
+    };
+
+    const response = {
+      code: 200,
+      data: result,
+    };
+    res.json(response);
+  } catch (err) {
+    const response = {
+      code: 400,
+      error: err,
+    };
+    res.status(400).json(response);
   }
-  const { currentDate } = value;
-
-  const nextDay = 1 * 24 * 60 * 60000;
-  const month = currentDate.getMonth() + 1;
-
-  const dailyTotalWeight = await Order.aggregate([
-    {
-      $match: {
-        orderDate: {
-          $gte: currentDate,
-          $lte: new Date(currentDate.getTime() + nextDay),
-        },
-      },
-    },
-    { $unwind: "$trashDetail" },
-    {
-      $group: {
-        _id: {
-          $dateToString: { format: "%m-%d-%Y", date: "$orderDate" },
-        },
-        sumWeight: { $sum: "$trashDetail.category" },
-      },
-    },
-  ]);
-
-  const dailyTotalFinishedWeight = await Order.aggregate([
-    {
-      $match: {
-        orderDate: {
-          $gte: currentDate,
-          $lte: new Date(currentDate.getTime() + nextDay),
-        },
-        orderStatus: "FINISH",
-      },
-    },
-    { $unwind: "$trashDetail" },
-    {
-      $group: {
-        _id: {
-          $dateToString: { format: "%m-%d-%Y", date: "$orderDate" },
-        },
-        sumWeight: { $sum: "$trashDetail.category" },
-      },
-    },
-  ]);
-
-  const dailyTotalInorganicWeight = await Order.aggregate([
-    {
-      $match: {
-        orderDate: {
-          $gte: currentDate,
-          $lte: new Date(currentDate.getTime() + nextDay),
-        },
-      },
-    },
-    { $unwind: "$trashDetail" },
-    {
-      $match: {
-        "trashDetail.type": {
-          $ne: "Organik",
-        },
-      },
-    },
-    {
-      $group: {
-        _id: {
-          $dateToString: { format: "%m-%d-%Y", date: "$orderDate" },
-        },
-        sumWeight: { $sum: "$trashDetail.category" },
-      },
-    },
-  ]);
-
-  const monthlyTotalWeight = await Order.aggregate([
-    { $unwind: "$trashDetail" },
-    {
-      $group: {
-        _id: {
-          $month: "$orderDate",
-        },
-        sumWeight: { $sum: "$trashDetail.category" },
-      },
-    },
-    {
-      $match: {
-        _id: month,
-      },
-    },
-  ]);
-
-  const monthlyTotalFinishedWeight = await Order.aggregate([
-    {
-      $match: {
-        orderStatus: "FINISH",
-      },
-    },
-    { $unwind: "$trashDetail" },
-    {
-      $group: {
-        _id: {
-          $month: "$orderDate",
-        },
-        sumWeight: { $sum: "$trashDetail.category" },
-      },
-    },
-    {
-      $match: {
-        _id: month,
-      },
-    },
-  ]);
-
-  const monthlyTotalInorganicWeight = await Order.aggregate([
-    { $unwind: "$trashDetail" },
-    {
-      $match: {
-        "trashDetail.type": {
-          $ne: "Organik",
-        },
-      },
-    },
-    {
-      $group: {
-        _id: {
-          $month: "$orderDate",
-        },
-        sumWeight: { $sum: "$trashDetail.category" },
-      },
-    },
-    {
-      $match: {
-        _id: month,
-      },
-    },
-  ]);
-
-  const result = {
-    daily: {
-      totalWeight: dailyTotalWeight.length ? dailyTotalWeight[0].sumWeight : 0,
-      totalFinishedWeight: dailyTotalFinishedWeight.length
-        ? dailyTotalFinishedWeight[0].sumWeight
-        : 0,
-      totalInorganicWeight: dailyTotalInorganicWeight.length
-        ? dailyTotalInorganicWeight[0].sumWeight
-        : 0,
-    },
-    monthly: {
-      totalWeight: monthlyTotalWeight.length
-        ? monthlyTotalWeight[0].sumWeight
-        : 0,
-      totalFinishedWeight: monthlyTotalFinishedWeight.length
-        ? monthlyTotalFinishedWeight[0].sumWeight
-        : 0,
-      totalInorganicWeight: monthlyTotalInorganicWeight.length
-        ? monthlyTotalInorganicWeight[0].sumWeight
-        : 0,
-    },
-  };
-
-  const response = {
-    code: 200,
-    data: result,
-  };
-  res.json(response);
-  // } catch (err) {
-  //   const response = {
-  //     code: 400,
-  //     error: err,
-  //   };
-  //   res.status(400).json(response);
-  // }
 };
 
 const dailyTotalPricePerType = async (req, res) => {
@@ -255,7 +257,7 @@ const dailyTotalPricePerType = async (req, res) => {
       6: 2000,
     };
 
-    let result = await Order.aggregate([
+    const currentDateOrder = await Order.aggregate([
       {
         $match: {
           orderDate: {
@@ -265,29 +267,19 @@ const dailyTotalPricePerType = async (req, res) => {
         },
       },
       { $unwind: "$trashDetail" },
-      {
-        $group: {
-          _id: "$trashDetail.category",
-          count: { $count: {} },
-        },
-      },
-      {
-        $addFields: {
-          category: "$_id",
-        },
-      },
-      {
-        $project: { _id: 0 },
-      },
-      {
-        $sort: { category: 1 },
-      },
     ]);
 
-    result = result.map((res) => ({
-      category: res.category,
-      sumPrice: res.count * pricePerCategory[res.category],
-    }));
+    const result = {};
+
+    currentDateOrder.forEach((order) => {
+      if (result.hasOwnProperty(order.trashDetail.type)) {
+        result[order.trashDetail.type] +=
+          pricePerCategory[order.trashDetail.category];
+      } else {
+        result[order.trashDetail.type] =
+          pricePerCategory[order.trashDetail.category];
+      }
+    });
 
     const response = {
       code: 200,
